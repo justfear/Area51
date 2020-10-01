4b8b6c08005b75f2a9303df636860a0b676f313f
justfear <lucmonne@gmail.com>
2020-10-01 12:04:57 +0200

    Remove bayespam, add Kmeans and kohonen.py

---
+++ kmeans.py
@@ -0,0 +1,63 @@
+"""kmeans.py"""
+
+
+class Cluster:
+    """This class represents the clusters, it contains the
+    prototype (the mean of all it's members) and memberlists with the
+    ID's (which are Integer objects) of the datapoints that are member
+    of that cluster. You also want to remember the previous members so
+    you can check if the clusters are stable."""
+
+    def __init__(self, dim):
+        self.prototype = [0.0 for _ in range(dim)]
+        self.current_members = set()
+        self.previous_members = set()
+
+
+class KMeans:
+    def __init__(self, k, traindata, testdata, dim):
+        self.traindata = traindata
+        self.testdata = testdata
+        self.dim = dim
+
+        # Threshold above which the corresponding html is prefetched
+        self.prefetch_threshold = 0.5
+        # An initialized list of k clusters
+        self.clusters = [Cluster(dim) for _ in range(k)]
+
+        # The accuracy and hitrate are the performance metrics (i.e. the results)
+        self.accuracy = 0
+        self.hitrate = 0
+
+    def train(self):
+        # implement k-means algorithm here:
+        # Step 1: Select an initial random partioning with k clusters
+        # Step 2: Generate a new partition by assigning each datapoint to its closest cluster center
+        # Step 3: recalculate cluster centers
+        # Step 4: repeat until clustermembership stabilizes
+        pass
+
+    def test(self):
+        # iterate along all clients. Assumption: the same clients are in the same order as in the testData
+        # for each client find the cluster of which it is a member
+        # get the actual testData (the vector) of this client
+        # iterate along all dimensions
+        # and count prefetched htmls
+        # count number of hits
+        # count number of requests
+        # set the global variables hitrate and accuracy to their appropriate value
+        pass
+
+    def print_test(self):
+        print("Prefetch threshold =", self.prefetch_threshold)
+        print("Hitrate:", self.hitrate)
+        print("Accuracy:", self.accuracy)
+        print("Hitrate+Accuracy =", self.hitrate + self.accuracy)
+
+    def print_members(self):
+        for i, cluster in enumerate(self.clusters):
+            print("Members cluster", i, ":", cluster.current_members)
+
+    def print_prototypes(self):
+        for i, cluster in enumerate(self.clusters):
+            print("Prototype cluster", i, ":", cluster.prototype)
---
+++ kohonen.py
@@ -0,0 +1,64 @@
+class Cluster:
+    """This class represents the clusters, it contains the
+    prototype (the mean of all it's members) and memberlists with the
+    ID's (which are Integer objects) of the datapoints that are member
+    of that cluster."""
+    def __init__(self, dim):
+        self.prototype = [0.0 for _ in range(dim)]
+        self.current_members = set()
+
+class Kohonen:
+    def __init__(self, n, epochs, traindata, testdata, dim):
+        self.n = n
+        self.epochs = epochs
+        self.traindata = traindata
+        self.testdata = testdata
+        self.dim = dim
+
+        # A 2-dimensional list of clusters. Size == N x N
+        self.clusters = [[Cluster(dim) for _ in range(n)] for _ in range(n)]
+        # Threshold above which the corresponding html is prefetched
+        self.prefetch_threshold = 0.5
+        self.initial_learning_rate = 0.8
+        # The accuracy and hitrate are the performance metrics (i.e. the results)
+        self.accuracy = 0
+        self.hitrate = 0
+
+    def train(self):
+        # Step 1: initialize map with random vectors (A good place to do this, is in the initialisation of the
+        # clusters) Repeat 'epochs' times:
+        # Step 2: Calculate the squareSize and the learningRate, these decrease
+        # lineary with the number of epochs.
+        # Step 3: Every input vector is presented to the map (always in the same
+        # order) For each vector its Best Matching Unit is found, and :
+        # Step 4: All nodes within the neighbourhood of the BMU are changed,
+        # you don't have to use distance relative learning. Since training kohonen maps can take
+        # quite a while, presenting the user with a progress bar would be nice
+        pass
+
+    def test(self):
+        # iterate along all clients
+        # for each client find the cluster of which it is a member
+        # get the actual testData (the vector) of this client
+        # iterate along all dimensions
+        # and count prefetched htmls
+        # count number of hits
+        # count number of requests
+        # set the global variables hitrate and accuracy to their appropriate value
+        pass
+
+    def print_test(self):
+        print("Prefetch threshold =", self.prefetch_threshold)
+        print("Hitrate:", self.hitrate)
+        print("Accuracy:", self.accuracy)
+        print("Hitrate+Accuracy =", self.hitrate+self.accuracy)
+
+    def print_members(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                print("Members cluster", (i, j), ":", self.clusters[i][j].current_members)
+
+    def print_prototypes(self):
+        for i in range(self.n):
+            for j in range(self.n):
+               print("Prototype cluster", (i, j), ":", self.clusters[i][j].prototype)
