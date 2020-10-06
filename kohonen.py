22722dced5ee66436459ba2e651b54b35777bf0c
justfear <lucmonne@gmail.com>
2020-10-06 17:52:06 +0200

    Kohonen

--- kohonen.py
+++ kohonen.py
@@ -114,6 +114,6 @@ class Kohonen:
             for row in two_d_matrix:
                 for element in row:
                     distance_matrix.append(distance(vector, element))
-            best_idx = distance_matrix.index(min(distance_matrix))
+            best_idx = distance_matrix.index(min(distance_matrix)) + 1
             idx_1, idx_2 = find_2D_index(best_idx, self.n)
             results_matrix.append(two_d_matrix[idx_1][idx_2])
